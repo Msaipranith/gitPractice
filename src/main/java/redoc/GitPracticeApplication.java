4e43dbba37398c140a8f17b4470c1ba7cd3a3570
Mugala Sai pranith <saipranithsunny@gmail.com>
2023-07-25 11:11:02 +0530

    updating to maven project

---
+++ src/main/java/redoc/GitPracticeApplication.java
@@ -0,0 +1,13 @@
+package redoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/redoc/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package redoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
