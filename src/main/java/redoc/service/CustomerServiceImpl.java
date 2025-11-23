c9ab3ddd4ded5753baa4535e5739430be1b9e3eb
Mugala Sai pranith <94050683+Msaipranith@users.noreply.github.com>
2025-11-23 16:21:09 +0530

    Update CustomerServiceImpl.java

--- src/main/java/redoc/service/CustomerServiceImpl.java
+++ src/main/java/redoc/service/CustomerServiceImpl.java
@@ -28,9 +28,6 @@ public class CustomerServiceImpl implements CustomerService {
 		return customerRepo.findAll();
 	}
 
-	@Override
-	public Customer getCustomerById(int id) {
-		return customerRepo.findById(id);
-	}
+
 
 }
